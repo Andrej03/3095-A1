da0743de876c7fd88c12bbd0a058a7704ce6b56c
Andrej03 <96391874+Andrej03@users.noreply.github.com>
2024-11-06 18:23:45 -0500

    localhost management, attempt to fix RoomClient not done

--- booking-service/src/main/java/ca/gbc/bookingservice/client/RoomClient.java
+++ booking-service/src/main/java/ca/gbc/bookingservice/client/RoomClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "room-service", url = "${room.service.url}")
+@FeignClient(value = "room-service", url = "https://localhost:8089")
 public interface RoomClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/api/rooms/available")
