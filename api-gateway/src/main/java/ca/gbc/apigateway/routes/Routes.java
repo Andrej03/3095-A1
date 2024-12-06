4da675eb4bb7ce7baa86c2ac5d97eeee00014d1b
Andrej03 <96391874+Andrej03@users.noreply.github.com>
2024-12-06 15:47:28 -0500

    keycloak and openapi setup

--- api-gateway/src/main/java/ca/gbc/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/ca/gbc/apigateway/routes/Routes.java
@@ -1,4 +1,81 @@
 package ca.gbc.apigateway.routes;
 
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.servlet.function.support.HandlerFunctionAdapter;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+
+@Configuration
+@Slf4j
 public class Routes {
+
+    @Value("${service.user-url}")
+    private String userServiceUrl;
+    @Value("${service.event-url}")
+    private String eventServiceUrl;
+    @Value("${service.room-url}")
+    private String roomServiceUrl;
+    @Value("${service.booking-url}")
+    private String bookingServiceUrl;
+    @Value("${service.approval-url}")
+    private String approvalServiceUrl;
+
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceRoute(HandlerFunctionAdapter handlerFunctionAdapter) {
+
+        log.info("Initializing product-service route with URL {}", userServiceUrl);
+
+        return route("user_service")
+                .route(RequestPredicates.path("/api/user"), request -> {
+
+                    log.info("Received request for user-service : {}", request.uri());
+
+                    try {
+                        ServerResponse response = HandlerFunctions.http(userServiceUrl).handle(request);
+                        log.info("Response status for user-service : {}", response.statusCode());
+                        return response;
+                    } catch (Exception e) {
+                        log.error("Error while handling request for user-service : {}", e.getMessage(), e);
+                        return ServerResponse.status(500).body("Error occurred when routing user-service");
+                    }
+                })
+                .filter(CircuitBreakerFilterFunctions
+                        .circuitBreaker("userServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> eventServiceRoute(HandlerFunctionAdapter handlerFunctionAdapter) {
+
+        log.info("Initializing event-service route with URL {}", eventServiceUrl);
+
+        return route("event_service")
+                .route(RequestPredicates.path("/api/event"), request -> {
+
+                    log.info("Received request for event-service : {}", request.uri());
+
+                    try {
+                        ServerResponse response = HandlerFunctions.http(eventServiceUrl).handle(request);
+                        log.info("Response status for event-service : {}", response.statusCode());
+                        return response;
+                    } catch (Exception e) {
+                        log.error("Error while handling request for event-service : {}", e.getMessage(), e);
+                        return ServerResponse.status(500).body("Error occurred when routing event-service");
+                    }
+                })
+                .filter(CircuitBreakerFilterFunctions
+                        .circuitBreaker("eventServiceCircuitBreaker", URI.create("forward:/fallbackRoute")))
+                .build();
+    }
 }
--- event-service/src/main/java/ca/gbc/eventservice/config/OpenAPIConfig.java
+++ event-service/src/main/java/ca/gbc/eventservice/config/OpenAPIConfig.java
@@ -1,4 +1,30 @@
 package ca.gbc.eventservice.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenAPIConfig {
+
+    @Value("${event-service-version}")
+    private String version;
+
+    @Bean
+    public OpenAPI eventServiceAPI() {
+
+        return new OpenAPI()
+                .info(new Info().title("Event Service API")
+                        .description("This is the rest API for Event Service")
+                        .version(version)
+                        .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("This is the rest API for Event Service")
+                        .url("https://mycompany.ca/event-service/docs"));
+    }
+
 }
