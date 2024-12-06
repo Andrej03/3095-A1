57cbce95c6c327b9d89629118847c6e1264a1b89
Andrej03 <96391874+Andrej03@users.noreply.github.com>
2024-12-06 18:54:34 -0500

    kafka, urls, clients and restclientconfig

---
+++ approval-service/src/main/java/ca/gbc/approvalservice/config/RestClientConfig.java
@@ -0,0 +1,41 @@
+package ca.gbc.approvalservice.config;
+
+import ca.gbc.approvalservice.client.BookingClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${booking.service.url}")
+    private String bookingServiceUrl;
+
+    @Bean
+    public BookingClient bookingClient() {
+
+        RestClient restClient = RestClient.builder()
+                .baseUrl(bookingServiceUrl)
+                .requestFactory(getClientFactory())
+                .build();
+
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(BookingClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
+    }
+}
